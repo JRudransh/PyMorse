6ec95769d89dfb9bda3125c4b9ca894374d914ad
JRudransh <JRudransh@protonmail.com>
2021-09-20 00:21:09 +0530

    Changed audio module

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
 #!/usr/bin/python3
 
 import numpy as np
-import simpleaudio as sa # pip3 install simpleaudio
 from time import sleep
+import time
+from pysinewave import SineWave
 
-spd = input('Set speed [1-20]: ')
+# level = input('Set Level [1-7]: ')
+level = '1'
+
+if level == '1':
+    spd = 10
+    g_u = 3
+elif level == '2':
+    spd = 20
+    g_u = 3
+elif level == '3':
+    spd = 25
+    g_u = 2
+elif level == '4':
+    spd = 40
+    g_u = 2
+elif level == '5':
+    spd = 80
+    g_u = 2
+elif level == '6':
+    spd = 80
+    g_u = 1
+elif level == '7':
+    spd = 130
+    g_u = 1
+
+
+
+
+
+
 
 if spd != '':
     try:
@@ -14,49 +44,68 @@ if spd != '':
 else:
     spd = 6
 
-unit = 1/6
-A_freq = 230
-sample_rate = 44100
+unit = 1/spd
 
 def sound(T):
-    n = int(sample_rate * T)
-    t = np.linspace(0, T, n, endpoint=True)
-    A_note = np.sin(A_freq * t * 2 * np.pi)
-    audio = np.hstack(A_note)
-    audio *= 32767 / np.max(np.abs(audio))
-    audio = audio.astype(np.int16)
-    play_obj = sa.play_buffer(audio, 2, 2, sample_rate)
-    play_obj.wait_done()
+    sinewave = SineWave(pitch = 7, pitch_per_second = 200)
+    # Turn the sine wave on.
+    sinewave.play()
+
+    # Sleep for 2 seconds, as the sinewave keeps playing.
+    time.sleep(T)
+    sinewave.stop()
 
 
-def dot(unit):
+
+def gap():
+    T = unit
+    sleep(T)
+
+
+def dot():
     T = unit
     sound(T)
+    gap()
     
 
-def dash(unit):
+def dash():
     T = unit * 3
     sound(T)
-    
+    gap()
 
-def gap(unit):
-    T = unit * 3
+
+def point():
+    T = unit * 2 * g_u
     sleep(T)
 
-def space(unit):
-    T = unit * 7
+
+def space():
+    T = unit * 4 * g_u
     sleep(T)
 
-code = {'A': [0, 1], 'B': [1, 0, 0, 0], 'C': [1, 0, 1, 0], 'D': [1, 0, 0], 'E': [0], 'F': [0, 0, 1, 0], 'G': [1, 1, 0], 'H': [0, 0, 0, 0], 'I': [0, 0], 'J': [0, 1, 1, 1], 'K': [1, 0, 1], 'L': [0, 1, 0, 0], 'M': [1, 1], 'N': [1, 0], 'O': [1, 1, 1], 'P': [0, 1, 1, 0], 'Q': [1, 1, 0, 1], 'R': [0, 1, 0], 'S': [0, 0, 0], 'T': [1], 'U': [0, 0, 1], 'V': [0, 0, 0, 1], 'W': [0, 1, 1], 'X': [1, 0, 0, 1], 'Y': [1, 0, 1, 1], 'Z': [1, 1, 0, 0]}
+
+code = {
+    'A': [0, 1], 'B': [1, 0, 0, 0], 'C': [1, 0, 1, 0], 'D': [1, 0, 0], 
+    'E': [0], 'F': [0, 0, 1, 0], 'G': [1, 1, 0], 'H': [0, 0, 0, 0], 
+    'I': [0, 0], 'J': [0, 1, 1, 1], 'K': [1, 0, 1], 'L': [0, 1, 0, 0], 
+    'M': [1, 1], 'N': [1, 0], 'O': [1, 1, 1], 'P': [0, 1, 1, 0], 
+    'Q': [1, 1, 0, 1], 'R': [0, 1, 0], 'S': [0, 0, 0], 'T': [1], 
+    'U': [0, 0, 1], 'V': [0, 0, 0, 1], 'W': [0, 1, 1], 'X': [1, 0, 0, 1], 
+    'Y': [1, 0, 1, 1], 'Z': [1, 1, 0, 0], '.' : [0,1,0,1,0,1], 
+    ',': [1, 1, 0, 0, 1, 1], '1': [0, 1, 1, 1, 1], '2': [0, 0, 1, 1, 1],
+    '3': [0, 0, 0, 1, 1], '4': [0, 0, 0, 0, 1], '5': [0, 0, 0, 0, 0],
+    '6': [1, 0, 0, 0, 0], '7': [1, 1, 0, 0, 0], '8': [1, 1, 1, 0, 0],
+    '9': [1, 1, 1, 1, 0]
+    }
 
 
 def play_char(data):
     cod = code[data]
     for i in cod:
         if i == 0:
-            dot(unit)
+            dot()
         else:
-            dash(unit)
+            dash()
 
 def print_char(data):
     print(data, end='  ')
@@ -73,17 +122,30 @@ def start(string):
     for i in string.upper():
         if i == ' ':
             print()
-            space(unit)
+            space()
         else:
-            gap(unit)
             print_char(i)
             play_char(i)
+            point()
     print('\n\n')
 
 
+def start2(string):
+    print('\n\n')
+    for word in string.split():
+        print(word)
+        for i in word.upper():
+            play_char(i)
+            point()
+        space()
+        
+    print('\n\n')
+
+
+
 try:
-    start('abcdefghijklmnopqrstuvwxyz')
-except KeyboardInterrupt:
+    start2('Hello Morse')
+except:
     pass
 
 while True:
